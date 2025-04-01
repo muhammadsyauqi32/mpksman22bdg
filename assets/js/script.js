f1bce0abb7a8c5a75ef5f83819e87f47780eb88e
muhammadsyauqi32 <baimb2822@gmail.com>
2025-04-01 13:02:04 +0700

    Add Member and About Page

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.body.classList.add("loaded");
+// });
+
+// document.addEventListener("click", function (event) {
+//   if (event.target.tagName === "A" && event.target.href) {
+//     event.preventDefault();
+//     document.body.style.opacity = "0";
+//     setTimeout(() => {
+//       window.location.href = event.target.href;
+//     }, 500);
+//   }
+// });
